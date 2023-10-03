cdeb5a24e9557b51c4d20eb5472ae8ad2696bfb1
unknown <eduardbh2014@gmail.com>
2023-10-03 12:11:15 -0300

    Inicio dos DTOs

---
+++ code/lagos/dto/src/main/java/br/cefetmg/lagos/model/dto/UsuarioLoja.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.cefetmg.lagos.model.dto;
+
+/**
+ *
+ * @author Aluno
+ */
+public class UsuarioLoja {
+    
+}
